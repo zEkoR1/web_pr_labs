57134aa84e396c46703226c628f874045d975b12
zEkoR1 <komarovitstudies@gmail.com>
2024-10-06 15:29:24 +0300

    Added logic for scrapping data from the inner link.

--- LAB_1.py
+++ LAB_1.py
@@ -1,28 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = "https://ultra.md/ru/category/smartphones"
+url = "https://ultra.md/category/smartphones"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-all_links = soup.find_all('a', class_= 'product-text pt-4 font-semibold text-gray-900 transition duration-200 hover:text-red-500 dark:text-white sm:text-sm')
-all_prices = soup.find_all('span', class_= 'text-blue text-xl font-bold dark:text-white')
+
+all_links = soup.find_all('a', class_='product-text pt-4 font-semibold text-gray-900 transition duration-200 hover:text-red-500 dark:text-white sm:text-sm')
+all_prices = soup.find_all('span', class_='text-blue text-xl font-bold dark:text-white')
 
 for link, price in zip(all_links, all_prices):
     link_text = link.text.strip()
     link_href = link.get('href')
-    # print (link_href)
+
     product_response = requests.get(link_href)
-
     product_soup = BeautifulSoup(product_response.text, 'html.parser')
+    memory_label = product_soup.find('h3', string="Display")
 
-    memory = product_soup.find('span', class_='font-bold text-gray-800')
+    if memory_label:
+        # memory_value_span = memory_label.find('span', class_='font-bold text-gray-800')
+        # memory = memory_value_span.text.strip() if memory_value_span else "N/A"
+        print('mn')
+    else:
+        # memory = "N/A"
+        print ("vvvv")
     price_text = price.text.strip()
-
-    print(f"Link Text: {link_text}, URL: {link_href}, Price: {price_text} Memory: {memory}")
-
-
-
-# print(soup.text)
-# print (link_text, link_href)
-
+    print(f"Link Text: {link_text}, URL: {link_href}, Price: {price_text}, ")
